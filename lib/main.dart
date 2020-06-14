911e8ab527eb02b153456750c0ee3e745d364a1f
brokoli <bocalist@gmail.com>
2020-06-15 00:23:41 +0900

    change appbar color and add some body text

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,12 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext) {
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello World', style: TextStyle(
-        fontSize: 30.5,
-        color: Colors.green[400]
-      ))
-    );
+      theme: ThemeData(primaryColor: Colors.purple[900]),
+      home: Scaffold(
+        appBar: AppBar(title: Text('WordPair Generator')), 
+        body: Center(child: Text('Hello World'),),
+    ));
   }
 }
