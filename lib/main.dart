9c0e2a9659fd10b49a165839de0f6f5715a1d7ff
brokoli <bocalist@gmail.com>
2020-06-15 00:44:04 +0900

    generate wordpair listview

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.purple[900]),
       home: RandomWords());
@@ -19,11 +18,37 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _randomWordPairs = <WordPair>[];
+
+  Widget _buildList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (context, item) {
+        if(item.isOdd) return Divider();
+
+        final index = item ~/ 2;
+
+        if(index >= _randomWordPairs.length) {
+          _randomWordPairs.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_randomWordPairs[index]);
+      }
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(pair.asPascalCase, 
+      style: TextStyle(fontSize: 18)));
+  }
+
   Widget build(BuildContext contenxt) {
     return Scaffold(
       appBar: AppBar(
         title: Text('WordPair Generator')
       ),
+      body: _buildList(),
     );
   }
 }
