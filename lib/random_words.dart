bb5375f4812bb724490a55bcf549ea08e6ce39a7
brokoli <bocalist@gmail.com>
2020-06-15 21:40:41 +0900

    add favorite buttons

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -8,6 +8,7 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _randomWordPairs = <WordPair>[];
+  final _savedWordPairs = Set<WordPair>();
 
   Widget _buildList() {
     return ListView.builder(
@@ -27,9 +28,26 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _savedWordPairs.contains(pair);
+
     return ListTile(
       title: Text(pair.asPascalCase, 
-      style: TextStyle(fontSize: 18)));
+        style: TextStyle(fontSize: 18)
+        ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null
+        ),
+      onTap: () {
+        setState(() {
+          if(alreadySaved) {
+            _savedWordPairs.remove(pair);
+          } else {
+            _savedWordPairs.add(pair);
+          }
+        });
+      }
+    );
   }
 
   Widget build(BuildContext contenxt) {
