d0a4e085dc67aa0480a0c3bff99825aa8059dedc
brokoli <bocalist@gmail.com>
2020-06-15 21:50:36 +0900

    add SavedWords page

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -50,10 +50,42 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _savedWordPairs.map((WordPair pair) {
+            return ListTile(
+              title: Text(pair.asPascalCase, style: TextStyle(fontSize: 16))
+            );
+          });
+
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved WordPairs'),
+            ),
+            body: ListView(children: divided)
+          );
+        }
+      )
+    );
+  }
+
   Widget build(BuildContext contenxt) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('WordPair Generator')
+        title: Text('WordPair Generator'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: _pushSaved,
+          )
+        ],
       ),
       body: _buildList(),
     );
